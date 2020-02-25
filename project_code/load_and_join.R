1b190efcbed70c45df3c2939c8b6c2f03e1f6b4a
danjwalton <20973540+danjwalton@users.noreply.github.com>
2020-02-25 10:40:27 +0000

    fread where csv

--- project_code/load_and_join.R
+++ project_code/load_and_join.R
@@ -4,18 +4,21 @@ load_crs <- function(dataname="crs", path="project_data"){
   files.csv <- list.files(path, pattern=paste0(dataname, "_part.[.]csv"))
   if(length(files.bz) > 0 & length(files.csv) > 0){
     files <- files.csv
+    read.crs <- function(x){return(fread(x))}
   } else {
     if(length(files.bz) > 0){
       files <- files.bz
+      read.crs <- function(x){return(read.csv(x))}
     } else {
       files <- files.csv
+      read.crs <- function(x){return(fread(x))}
     }
   }
   crs <- list()
   for(i in 1:length(files)){
     print(paste0("Loading part ", i, " of ", length(files)))
     filepath <- paste0(path, "/", files[i])
-    crs[[i]] <- read.csv(filepath)
+    crs[[i]] <- read.crs(filepath)
   }
   crs <- rbindlist(crs)
   return(crs)
