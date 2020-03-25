4d04b5908d7906e586c91ed811caa84515a1103f
danjwalton <20973540+danjwalton@users.noreply.github.com>
2020-03-25 12:57:21 +0000

    UTF encoding

--- project_code/load_and_join.R
+++ project_code/load_and_join.R
@@ -4,21 +4,21 @@ load_crs <- function(dataname="crs", path="project_data"){
   files.csv <- list.files(path, pattern=paste0(dataname, "_part.+[.]csv"))
   if(length(files.bz) > 0 & length(files.csv) > 0){
     files <- files.csv
-    read.crs <- function(x){return(fread(x))}
+    read.crs <- function(x){return(fread(x, encoding = 'UTF-8'))}
   } else {
     if(length(files.bz) > 0){
       files <- files.bz
-      read.crs <- function(x){return(read.csv(x))}
+      read.crs <- function(x){return(read.csv(x, encoding = 'UTF-8'))}
     } else {
       files <- files.csv
-      read.crs <- function(x){return(fread(x))}
+      read.crs <- function(x){return(fread(x, encoding = 'UTF-8'))}
     }
   }
   crs <- list()
   for(i in 1:length(files)){
     print(paste0("Loading part ", i, " of ", length(files)))
     filepath <- paste0(path, "/", files[i])
-    crs[[i]] <- read.crs(filepath)
+    crs[[i]] <- read.crs(filepath, encoding = 'UTF-8')
   }
   crs <- rbindlist(crs)
   return(crs)
